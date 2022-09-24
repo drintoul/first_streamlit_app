231aae6b03dea5dae858996428f1c2d001109d71
Dave Rintoul <drintoul@gmail.com>
2022-09-24 13:42:48 -0400

    Update streamlit_app.py
    updated emoticons

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import streamlit
 streamlit.title('My Mom\'s New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
-streamlit.text('\N{bowl}Omega 3 & Blueberry Oatmeal')
-streamlit.text('\N{kale}Kale, Spinach & Rocket Smoothie')
+streamlit.text('\N{stuffed flatbread}Omega 3 & Blueberry Oatmeal')
+streamlit.text('\N{green salad}Kale, Spinach & Rocket Smoothie')
 streamlit.text('\N{chicken}Hard-Boiled Free-Range Egg')
 streamlit.text('\N{avocodo}\N{toast}Avocoado Toast')
 
